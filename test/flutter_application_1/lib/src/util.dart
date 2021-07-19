6447947930014b77f2054873af3462d1a7ca88e0
kumangho <impapa68@gmail.com>
2021-07-19 17:29:30 +0900

    commit

--- test/flutter_application_1/lib/src/util.dart
+++ test/flutter_application_1/lib/src/util.dart
@@ -10,6 +10,8 @@ Color SubColor = Color(0xFFC8AA6E);
 ScrollController _scrollController;
 Color caughtColor = Colors.red;
 List _targetImage = List<String>.filled(10, null, growable: false);
+List _bluPlayer = List<String>.filled(5, null, growable: false);
+List _redPlayer = List<String>.filled(5, null, growable: false);
 int variableSet = 0;
 double width;
 double height;
@@ -82,33 +84,68 @@ BoxDecoration myTabBoxDecoration() {
 // ignore: non_constant_identifier_names
 Container PlayerContainer(int n) {
   return Container(
-    height: 65,
-    width: 105.7,
-    child: Stack(
-      children: [
-        Container(
-            decoration: BoxDecoration(
-                color: MainColor,
-                border: Border.all(color: SubColor),
-                image: DecorationImage(
-                    image: new AssetImage('assets/images/champion_icon.jpg'),
-                    fit: BoxFit.scaleDown))),
-        Positioned(
-          right: 5,
-          bottom: 15,
-          child: Container(
-            padding: EdgeInsets.all(10),
-            height: 20,
-            width: 20,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: new AssetImage(Champions.lines[n]),
-                    fit: BoxFit.scaleDown)),
-          ),
-        )
-      ],
-    ),
-  );
+      width: 105.7,
+      height: 65,
+      decoration: myBoxDecoration(),
+      child: DragTarget<String>(onAccept: (value) {
+        _bluPlayer[n] = value;
+      }, builder: (_, candidateData, rejectedData) {
+        return Stack(
+          children: [
+            Container(
+                width: 105.7,
+                height: 65,
+                alignment: Alignment.center,
+                child: _bluPlayer[n] != null //1)
+                    ? Image.asset(
+                        "assets/images/champions/TOP/RiotX_ChampionList_aatrox.jpg",
+                        fit: BoxFit.cover)
+                    : Container()),
+            Container(
+              child: LongPressDraggable(
+                data: _bluPlayer[n],
+                feedback: Container(
+                  child: Image.asset(
+                      "assets/images/champions/TOP/RiotX_ChampionList_aatrox.jpg", //2)
+                      fit: BoxFit.cover,
+                      height: 65,
+                      width: 105.7),
+                  decoration: myBoxDecoration(),
+                ),
+                child: Container(
+                    child: Image.asset(
+                        "assets/images/champions/TOP/RiotX_ChampionList_aatrox.jpg", //3)
+                        fit: BoxFit.cover,
+                        height: 65,
+                        width: 105.7),
+                    decoration: myBoxDecoration()),
+              ),
+              decoration: myBoxDecoration(),
+            ),
+            Container(
+                decoration: BoxDecoration(
+                    color: MainColor,
+                    border: Border.all(color: SubColor),
+                    image: DecorationImage(
+                        image:
+                            new AssetImage('assets/images/champion_icon.jpg'),
+                        fit: BoxFit.scaleDown))),
+            Positioned(
+              right: 5,
+              bottom: 15,
+              child: Container(
+                padding: EdgeInsets.all(10),
+                height: 20,
+                width: 20,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: new AssetImage(Champions.lines[n]),
+                        fit: BoxFit.scaleDown)),
+              ),
+            )
+          ],
+        );
+      }));
 }
 
 // ignore: non_constant_identifier_names
